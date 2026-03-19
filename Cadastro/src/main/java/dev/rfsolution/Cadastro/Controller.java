0c102c84826400968571d944df37285303bc1ef4
Gustavo Reis <suporte1@rfsolutionit.onmicrosoft.com>
2026-03-19 16:49:58 -0300

    Controller criada

---
+++ Cadastro/src/main/java/dev/rfsolution/Cadastro/Controller.java
@@ -0,0 +1,16 @@
+package dev.rfsolution.Cadastro;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String BoasVindas(){
+        return "Essa e minha primeira mensagem nessa rota";
+    }
+
+
+}
